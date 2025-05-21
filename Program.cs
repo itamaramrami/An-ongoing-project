1c62a63d1a7fd05620e6ef1731f5f72aaa02b699
itamar <ya0534516378@gmail.com>
2025-05-21 13:04:00 +0300

    idf

--- Program.cs
+++ Program.cs
@@ -10,7 +10,10 @@ namespace An_ongoing_project
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("itamar");
+            List<Weapon> nnn = new List<Weapon>();
+            f16 f1 = new f16();
+            f1.NumberOfBombs = 0;
+            Console.WriteLine(f1.IsAvailable());
         }
     }
 }
