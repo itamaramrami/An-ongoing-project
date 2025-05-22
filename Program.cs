6754d3eba912058c5eb0f0118aeb9b40b649a672
itamar <ya0534516378@gmail.com>
2025-05-22 16:15:43 +0300

    attack

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 
 namespace An_ongoing_project
 {
@@ -7,6 +8,22 @@ namespace An_ongoing_project
     {
         static void Main(string[] args)
         {
+
+            f16 m1 = new f16();
+           
+            drone d1 = new drone();
+            artillery a1 = new artillery();
+          
+            List <Weapon> helHiam1 = new List<Weapon>();
+            helHiam1.Add(m1);
+       
+            
+            helHiam1.Add(d1);
+            helHiam1.Add(a1);
+            IDFUnit helHiam = new IDFUnit("helHiam", "10,02,1960", helHiam1);
+
+
+
             GenerateRandomTerrorists(10);
 
             Console.WriteLine("=== All Hamas Terrorists ===\n");
@@ -18,14 +35,15 @@ namespace An_ongoing_project
                 Console.WriteLine(new string('-', 50));
             }
 
-            foreach (Terrorist terrorist in Hamas.GetHamasTerrorists())
-            {
-                Terrorist heighstTerrorist = Aman.GetTerroristHighestScore();
-                heighstTerrorist.KillTerrorist();
-            }
+            
+
 
             Console.WriteLine($"\nTotal terrorists in Hamas: {Hamas.GetHamasTerrorists().Count}");
+
+            helHiam.attack();
+            helHiam.attack();
         }
+
 
         static void GenerateRandomTerrorists(int count)
         {
@@ -50,6 +68,10 @@ namespace An_ongoing_project
 
                 new Terrorist(name, rank, new List<weapon>(weapons), loc);
             }
+           
         }
+        
+
     }
+    
 }
