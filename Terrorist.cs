e7d4a42b8f31e83bbf5119ffad7b8f852ea09e75
Haim Assraf <haimassraf@gmail.com>
2025-05-22 16:11:52 +0300

    d

--- Terrorist.cs
+++ Terrorist.cs
@@ -32,7 +32,30 @@ namespace An_ongoing_project
         }
         public List<weapon> GetTerroristWeapons() => this.TerroristWeapons;
         private static int GenerateNewId() => IdCounter++;
+         public static void GenerateRandomTerrorists(int count)
+        {
+            Random rand = new Random();
 
+            string[] sampleNames = { "Ali", "Khaled", "Omar", "Yusuf", "Hassan", "Samir", "Nabil", "Fadi", "Tariq", "Jamal" };
+            Array weaponValues = Enum.GetValues(typeof(weapon));
+            Array locationValues = Enum.GetValues(typeof(location));
+
+            for (int i = 0; i < count; i++)
+            {
+                string name = sampleNames[rand.Next(sampleNames.Length)];
+                int rank = rand.Next(1, 6);
+                var loc = (location)locationValues.GetValue(rand.Next(locationValues.Length));
+
+                var weaponCount = rand.Next(1, 5);
+                var weapons = new HashSet<weapon>();
+                while (weapons.Count < weaponCount)
+                {
+                    weapons.Add((weapon)weaponValues.GetValue(rand.Next(weaponValues.Length)));
+                }
+
+                new Terrorist(name, rank, new List<weapon>(weapons), loc);
+            }
+        }
 
         public Terrorist(string terroristName, int terroristRanke, List<weapon> terroristWeapons,location location, bool isAlive = true)
         {
